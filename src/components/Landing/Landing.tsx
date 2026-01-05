e554ebe7e05ba1a1f256a5e8fa3f8c792bbc6a08
Davey Walbeck <daveywalbeck@gmail.com>
2026-01-04 21:19:24 -0700

    swapped icons for images

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -19,20 +19,20 @@ const Landing: React.FC = () => {
               Draper, UT
             </p>
             <div className="mt-4 flex flex-row gap-2">
-              <a href={config.social.github} target="_blank" className="text-2xl mx-2 text-gray-600 hover:text-gray-600">
-                  <img src="/github.png" width="38" height="38" alt="GitHub" />
+              <a href={config.social.github} target="_blank">
+                  <img src="/github.png" width="38" height="38" alt="GitHub" className="hover:scale-110 transition-transform duration-300 ease-in-out focus:outline-none" />
               </a>
-              <a href={config.social.linkedin} target="_blank" className="text-2xl mx-2 text-gray-600 hover:text-gray-600">
-                  <img src="/linkedin.png" width="38" height="38" alt="LinkedIn" />
+              <a href={config.social.linkedin} target="_blank">
+                  <img src="/linkedin.png" width="38" height="38" alt="LinkedIn" className="hover:scale-110 transition-transform duration-300 ease-in-out focus:outline-none" />
               </a>
-              <a href={config.files.resume} target="_blank" rel="noopener noreferrer" className="text-2xl mx-2 text-gray-600 hover:text-gray-600">
-                  <img src="/cv.png" width="38" height="38" alt="Rsume" />
+              <a href={config.files.resume} target="_blank" rel="noopener noreferrer">
+                  <img src="/cv.png" width="38" height="38" alt="Rsume" className="hover:scale-110 transition-transform duration-300 ease-in-out focus:outline-none" />
               </a>
-              <a href="mailto:{config.contact.email}" target="_blank" rel="noopener noreferrer" className="text-2xl mx-2 text-gray-600 hover:text-gray-600">
-                  <img src="/email.png" alt="Email" width="38" height="38" />
+              <a href="mailto:{config.contact.email}" target="_blank" rel="noopener noreferrer">
+                  <img src="/email.png" alt="Email" width="38" height="38" className="hover:scale-110 transition-transform duration-300 ease-in-out focus:outline-none" />
               </a>
-              <a href="/davey_walbeck-executive_bio.pdf" target="_blank" rel="noopener noreferrer" className="text-2xl mx-2 text-gray-600 hover:text-gray-600">
-                  <img src="/report.png" alt="Executive Bio Report" width="38" height="38" />
+              <a href="/davey_walbeck-executive_bio.pdf" target="_blank" rel="noopener noreferrer">
+                  <img src="/report.png" alt="Executive Bio Report" width="38" height="38" className="hover:scale-110 transition-transform duration-300 ease-in-out focus:outline-none" />
               </a>
             </div>
           </div>
