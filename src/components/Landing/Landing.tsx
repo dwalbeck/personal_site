5f9bbbd049e65af5138c93a52204b863f5da362f
Davey Walbeck <daveywalbeck@gmail.com>
2026-01-04 21:50:09 -0700

    swapped icons for images

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -19,7 +19,7 @@ const Landing: React.FC = () => {
               Draper, UT
             </p>
               <div className="justify-center">
-                <div className="mt-4 flex flex-row gap-2 content-between">
+                <div className="mt-4 flex flex-row gap-8 content-between">
                   <a href={config.social.github} target="_blank">
                       <img src="/github.png" width="38" height="38" alt="GitHub" className="hover:scale-125 transition-transform duration-300 ease-in-out focus:outline-none" />
                   </a>
